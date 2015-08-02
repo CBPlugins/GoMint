d37da959d88fee83df12e34e9223363cd5d3dc96
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2015-08-02 13:55:02 +0200

    MIT License Header for each source file

--- gomint-server/src/main/java/io/gomint/server/plugin/SimplePluginManager.java
+++ gomint-server/src/main/java/io/gomint/server/plugin/SimplePluginManager.java
@@ -1,3 +1,9 @@
+/*
+ *  Copyright (c) 2015, GoMint, BlackyPaw and geNAZt
+ *
+ *  This code is licensed under the MIT license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
 package io.gomint.server.plugin;
 
 import io.gomint.plugin.Plugin;
